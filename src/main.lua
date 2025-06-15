bf8e9874e8f8255e63dae77edc0cf31a45bd548f
Ricardo Casía <31012661+rcasia@users.noreply.github.com>
2025-06-15 11:56:52 +0200

    feat: add argparse dependency

--- src/main.lua
+++ src/main.lua
@@ -1,1 +1,11 @@
-print("Hello world!")
+local argparse = require("argparse")
+
+-- Create an argument parser
+local parser = argparse("script", "An example.")
+parser:argument("input", "Input file.")
+
+local args = parser:parse()
+
+-- Directly print out the "input" argument
+print(args.input)
+
