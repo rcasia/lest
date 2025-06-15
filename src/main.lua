046fe0cbf6bdf56573328158e274da9ea83eaf04
Ricardo Casía <31012661+rcasia@users.noreply.github.com>
2025-06-15 14:41:51 +0200

    refactor: put default values on argparse

--- src/main.lua
+++ src/main.lua
@@ -1,7 +1,6 @@
 local argparse = require("argparse")
 local mini = require("mini.test")
 
--- Create an argument parser
 local parser = argparse(
 	"lest",
 	[[
@@ -9,20 +8,17 @@ A CLI interface around echasnovski/mini.test
 for Neovim plugins developed as Lux Projects
 ]]
 )
-parser:option("--pattern", "Test pattern. Default: '**/*_spec.lua'")
-parser:option("-d --directory", "Test directory. Default: 'spec'")
+parser:option("--pattern", "Test pattern. Default: '**/*_spec.lua'", "**/*_spec.lua")
+parser:option("-d --directory", "Test directory. Default: 'spec'", "spec")
 
 local args = parser:parse()
 
 --- @param opts { pattern: string?, directory: string? }
 local function run(opts)
-	local pattern = opts.pattern or "**/*_spec.lua"
-	local directory = opts.directory or "spec"
-
 	mini.setup({
 		collect = {
 			find_files = function()
-				return vim.fn.globpath(directory, pattern, true, true)
+				return vim.fn.globpath(opts.directory, opts.pattern, true, true)
 			end,
 		},
 	})
