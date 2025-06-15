1cf0ce45a82a56e5daa700ac55285e4b728f1a34
Ricardo Casía <31012661+rcasia@users.noreply.github.com>
2025-06-15 14:29:59 +0200

    feat: add --pattern and --directory as options

--- src/main.lua
+++ src/main.lua
@@ -3,13 +3,24 @@ local mini = require("mini.test")
 
 -- Create an argument parser
 local parser = argparse("script", "An example.")
-parser:argument("input", "Input file.")
+parser:option("--pattern", "Test pattern. Default: '**/*_spec.lua'")
+parser:option("--directory", "Test directory. Default: 'spec'")
 
 local args = parser:parse()
 
--- Directly print out the "input" argument
-print(args.input)
+--- @param opts { pattern: string?, directory: string? }
+local function run(opts)
+	local pattern = opts.pattern or "**/*_spec.lua"
+	local directory = opts.directory or "spec"
 
-vim.iter({ "algunas", "palabras" }):each(function(w)
-	print(w)
-end)
+	mini.setup({
+		collect = {
+			find_files = function()
+				return vim.fn.globpath(directory, pattern, true, true)
+			end,
+		},
+	})
+	mini.run()
+end
+
+run({ pattern = args.pattern, directory = args.directory })
